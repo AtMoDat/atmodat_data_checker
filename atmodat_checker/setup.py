1fa791021b1e54aec4919256613cb85d31b922ec
Jan Kretzschmar <jan.kretzschmar@uni-leipzig.de>
2020-08-13 10:43:48 +0200

    Add cc-yaml and compliance-check-lib as submodules

---
+++ atmodat_checker/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                 = "atmodat_check_lib",
+    description          = "Compliance Check Library for AtMoDat Standard - python library of compliance checks",
+    author               = "xxx",
+    author_email         = "xxx@abc.de",
+    scripts              =[],
+    entry_points         = {},
+)
+
+
+
+# #reqs = [line.strip() for line in open('requirements.txt')]
+# #GIT_REPO = "https://github.com/cedadev/compliance-check-lib"
+
+# setup(
+#     name                 = "atmodat-check-lib",
+# #    version              = __version__,
+#     description          = "Compliance Check Library for AtMoDat Standard - python library of compliance checks",
+#     author               = "xxx",
+#     author_email         = "xxx@abc.de",
+# #    url                  = GIT_REPO,
+# #    packages             = find_packages(),
+# #    install_requires     = reqs,
+# #    tests_require        = ['pytest'],
+#     # classifiers          = [
+#     #     'Development Status :: 2 - ???',
+#     #     'Intended Audience :: Developers',
+#     #     'Intended Audience :: Science/Research',
+#     #     'License :: BSD 3-Clause License',
+#     #     'Operating System :: POSIX :: Linux',
+#     #     'Programming Language :: Python',
+#     #     'Topic :: Scientific/Engineering',
+#     # ],
+# #    include_package_data = True,
+#     scripts=[],
+#     entry_points         = {},
+# )
