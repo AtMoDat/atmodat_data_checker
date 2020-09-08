8a5d4bab63fc87162e605d3e90c40707d2d728c4
Jan Kretzschmar <jan.kretzschmar@uni-leipzig.de>
2020-09-08 09:25:35 +0200

    Add checker for ISO8601 conformity and some minor clean-up

--- atmodat_checker/atmodat_checklib/utils/nc_util.py
+++ atmodat_checker/atmodat_checklib/utils/nc_util.py
@@ -1,6 +1,8 @@
 import numpy as np
+import datetime
 
-def check_global_attr_type(ds, attr, attr_type, status):
+
+def check_global_attr_type(ds, attr, attr_type):
     """
     Checks globals attribute values in a NetCDF Dataset and returns integer regarding
     whether the `attr` matches `attr_type`.
@@ -10,10 +12,9 @@ def check_global_attr_type(ds, attr, attr_type, status):
     :param ds: netCDF4 Dataset object
     :param attr: global attribute name [string]
     :param attr_type: a numpy type [string]
-    :param status: status (mandatory or sth. else) [string]
     :return: Integer (0: not found; 1: found (incorrect type); 2: found and correct type.
     """
-    if  not attr in ds.ncattrs():
+    if attr not in ds.ncattrs():
         return 0
 
     global_attr = getattr(ds, attr)
@@ -24,7 +25,7 @@ def check_global_attr_type(ds, attr, attr_type, status):
     return 2
 
 
-def check_global_attr_ISO8601(ds, attr):
+def check_global_attr_iso8601(ds, attr):
     """
     Checks globals attribute values in a NetCDF Dataset and returns integer regarding
     whether the `attr` matches ISO8601 format.
@@ -33,9 +34,26 @@ def check_global_attr_ISO8601(ds, attr):
     :param attr: global attribute name [string]
     :return: Integer (0: incorrect format; 1: correct format).
     """
+
+    if attr not in ds.ncattrs():
+        return 0
+
     global_attr = getattr(ds, attr)
 
-    if np.dtype(type(global_attr)) != np.dtype(attr_type):
-        return 1
+    return check_iso8601_conformity(global_attr)
 
-    return 2
+
+def check_iso8601_conformity(date_string):
+
+    # Timezone related replacements
+    if date_string.endswith('Z'):
+        date_string = date_string.replace('Z', '+00:00')
+    # Replace comma with decimal point
+    if ',' in date_string:
+        date_string = date_string.replace(',', '.')
+
+    try:
+        datetime.datetime.fromisoformat(date_string)
+        return 2
+    except ValueError:
+        return 1
