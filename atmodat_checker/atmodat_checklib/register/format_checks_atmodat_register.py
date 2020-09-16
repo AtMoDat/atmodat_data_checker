c931e62e4f0180e1d55ed7a73df72b1691239c32
am-kaiser <kaiser@dkrz.de>
2020-09-16 15:23:20 +0200

    corrected output of ISO8601 check (i.e. without quote marks)

--- atmodat_checker/atmodat_checklib/register/format_checks_atmodat_register.py
+++ atmodat_checker/atmodat_checklib/register/format_checks_atmodat_register.py
@@ -14,23 +14,19 @@ from compliance_checker.base import Result
 from checklib.register.file_checks_register import FileCheckBase
 
 
-class NCFileIsReadableCheck(FileCheckBase):
+class FileIsNetCDF(FileCheckBase):
     """
-    Data file is recognised as a valid netCDF file, using sub-format: {file_format}.
+    Data file is recognised as a valid netCDF file.
     """
     short_name = "File is netCDF"
-    defaults = {"file_format": "NETCDF4_CLASSIC"}
-    message_templates = ["File is not in required netCDF format: {file_format}."]
+    message_templates = ["File is not in required netCDF format."]
     level = "HIGH"
 
     def _get_result(self, primary_arg):
         from netCDF4 import Dataset
 
         try:
-            ds = Dataset(primary_arg)
-            assert(type(ds.variables) == dict)
-            assert(type(ds.dimensions) == dict)
-            assert(ds.file_format == self.kwargs['file_format'])
+            ds = Dataset(self._get_filepath(primary_arg))
             success = True
         except Exception as err:
             success = False
@@ -46,41 +42,3 @@ class NCFileIsReadableCheck(FileCheckBase):
         return Result(self.level, (score, self.out_of),
                       self.get_short_name(), messages)
 
-
-class NCFileSoftwareCheck(FileCheckBase):
-    """
-    Data file is recognised as a valid netCDF file by multiple python packages (iris/xarray).
-    """
-    short_name = "File is netCDF readable by multiple packages"
-    defaults = {}
-    message_templates = ["File cannot be read by iris.",
-                         "File cannot be read by xarray."]
-    level = "HIGH"
-
-    def _get_result(self, primary_arg):
-        import iris
-        import xarray as xr
-
-        score = 0
-
-        try:
-            cl = iris.load(primary_arg)
-            assert(type(cl) in (iris.cube.Cube, iris.cube.CubeList))
-            score += 1
-        except Exception as err:
-            pass
-
-        try:
-            ds = xr.open_dataset(primary_arg)
-            assert(type(ds) in (xr.Dataset, xr.DataArray))
-            score += 1
-        except Exception as err:
-            pass
-
-        messages = []
-
-        if score < self.out_of:
-            messages.append(self.get_messages()[score])
-
-        return Result(self.level, (score, self.out_of),
-                      self.get_short_name(), messages)
