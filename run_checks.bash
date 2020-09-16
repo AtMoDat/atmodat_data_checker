c00ac8621c35cb68b09751b5572d15071acdba43
am-kaiser <kaiser@dkrz.de>
2020-09-16 15:55:23 +0200

    added CF checker to bash file

--- run_checks.bash
+++ run_checks.bash
@@ -5,3 +5,5 @@ ifile=$1
 cchecker.py --y atmodat_standard_checker_mandatory.yml --test atmodat_standard_checker_mandatory:1.0 ${ifile}
 echo "--------------------------------------------------------------------------------"
 cchecker.py --y atmodat_standard_checker_recommended.yml --test atmodat_standard_checker_recommended:1.0 ${ifile}
+echo "--------------------------------------------------------------------------------"
+cfchecks -v auto ${ifile}
