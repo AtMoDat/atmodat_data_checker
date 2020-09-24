32a8f91febb39fe6c1199c1056ac79d72f8d4bb1
am-kaiser <kaiser@dkrz.de>
2020-09-24 09:33:16 +0200

    added checks for optional global attributes
    (cherry picked from commit 734d857947dddd8c83888c5890c4b5ce568968b2)

--- run_checks.bash
+++ run_checks.bash
@@ -6,4 +6,6 @@ cchecker.py --y atmodat_standard_checker_mandatory.yml --test atmodat_standard_c
 echo "--------------------------------------------------------------------------------"
 cchecker.py --y atmodat_standard_checker_recommended.yml --test atmodat_standard_checker_recommended:1.0 ${ifile}
 echo "--------------------------------------------------------------------------------"
+cchecker.py --y atmodat_standard_checker_optional.yml --test atmodat_standard_checker_optional:1.0 ${ifile}
+echo "--------------------------------------------------------------------------------"
 cfchecks -v auto ${ifile}
