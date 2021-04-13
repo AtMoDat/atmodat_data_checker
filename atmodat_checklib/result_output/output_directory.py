d7907811f4f5057461b7db7b1dc689497b4de5d9
atmodatcode <79904507+atmodatcode@users.noreply.github.com>
2021-04-13 17:28:36 +0200

    Update output_directory.py
    removed personal check print

--- atmodat_checklib/result_output/output_directory.py
+++ atmodat_checklib/result_output/output_directory.py
@@ -13,8 +13,6 @@ now_formatted = now.strftime("%Y%m%d_%H%M") + '/'
 # Get parent of parent directory of current file
 opath_base = str(Path(__file__).resolve().parents[2])
 
-print('AH: opath_base=', opath_base)
-
 # Define output path for result files
 if opath_base:
     opath = opath_base + '/checker_output/' + now_formatted
