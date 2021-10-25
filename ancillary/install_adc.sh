4d5ea28fb45cca9280ec8a030a7101999fbaaaa3
Jan Kretzschmar <jan.kretzschmar@uni-leipzig.de>
2021-10-25 07:08:11 -0700

    Fix command line string size issue (#80)
    * Fix command line string size issue
    
    * Update linter.yml
    
    * Some linter related changes
    
    * Fix some linter issues

--- ancillary/install_adc.sh
+++ ancillary/install_adc.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-
-
-
 git clone https://github.com/AtMoDat/atmodat_data_checker.git
-cd atmodat_data_checker
+cd atmodat_data_checker || exit
 
 
 if [ $# -gt 0 ] ; then
@@ -12,12 +9,13 @@ if [ $# -gt 0 ] ; then
    sed -i.bak "s/atmodat/${new_env}/" environment.yml
 fi
 
-env_name=`sed -n 2p environment.yml |cut -d: -f2|tr -d ' '`
-echo "Installing atmodat checker in environment $env_name"
+env_name=$(sed -n 2p environment.yml |cut -d: -f2|tr -d ' ')
+echo "Installing atmodat checker in environment" "{$env_name}"
 
 conda env create         # environment name is retrieved from environment.yml
 
-source ~/anaconda3/bin/activate $env_name      #-- equivalent to conda activate ${env_name} in terminal
+# shellcheck disable=SC1090
+source ~/anaconda3/bin/activate "$env_name"      #-- equivalent to conda activate ${env_name} in terminal
 #source ~/anaconda3/etc/profile.d/conda.sh
 
 pip install -U -e .
@@ -28,6 +26,6 @@ git submodule update
 #=== install jupyterlab and create ipykernel with name of the environment
 
 mamba install --yes -c conda-forge jupyterlab
-python -m ipykernel install --user --name ${env_name}kernel
+python -m ipykernel install --user --name "{$env_name}"kernel
 
 
