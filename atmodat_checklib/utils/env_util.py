2d6414589756369baf2abfa52e75dd645f8d1b4c
Jan Kretzschmar <jan.kretzschmar@uni-leipzig.de>
2021-10-27 11:34:42 +0200

    Adapt setting environment variables for Windows

--- atmodat_checklib/utils/env_util.py
+++ atmodat_checklib/utils/env_util.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+import platform
 
 def set_env_variables():
 
@@ -11,10 +11,13 @@ def set_env_variables():
         atmodat_conda_envpath = os.environ["CONDA"]
 
     # UDUNITS2_XML_PATH
-    udunits2_xml_path_out = atmodat_conda_envpath + '/share/udunits/udunits2.xml'
+    if platform.system() == 'Linux' or platform.system() == 'Darwin':
+        udunits2_xml_path_out = os.path.join(atmodat_conda_envpath, 'share', 'udunits', 'udunits2.xml')
+    elif platform.system() == 'Windows':
+        udunits2_xml_path_out = os.path.join(atmodat_conda_envpath, 'Library', 'share', 'udunits', 'udunits2.xml')
 
-    # Set
+    # PYESSV_ARCHIVE_PATH
     atmodat_base_path = str(Path(__file__).resolve().parents[2])
-    pyessv_archive_home_out = atmodat_base_path + '/AtMoDat_CVs/pyessv-archive'
+    pyessv_archive_home_out = os.path.join(atmodat_base_path, 'AtMoDat_CVs', 'pyessv-archive')
 
     return udunits2_xml_path_out, pyessv_archive_home_out
