ca096588613b82b3c8e09d47f36f492100f6b2bc
Jan Kretzschmar <jan.kretzschmar@uni-leipzig.de>
2021-11-12 16:33:30 +0100

    Update test_run_checks.py after rebase and use tmpdir for demo data and checker output

--- atmodat_checklib/test/test_run_checks.py
+++ atmodat_checklib/test/test_run_checks.py
@@ -1,37 +1,24 @@
 import os
-import glob
 from run_checks import run_checks
 import atmodat_checklib.result_output.output_directory as output_directory
 from atmodat_checklib.result_output.summary_creation import extract_overview_output_json, \
     extracts_error_summary_cf_check
 import pandas as pd
+from atmodat_checklib.utils.env_util import set_env_variables
+from git import Repo
+from pathlib import Path
 
 
 prio_dict = {'high_priorities': 'Mandatory', 'medium_priorities': 'Recommended', 'low_priorities': 'Optional'}
+udunits2_xml_path, pyessv_archive_home = set_env_variables()
+os.environ['PYESSV_ARCHIVE_HOME'] = pyessv_archive_home
+os.environ['UDUNITS2_XML_PATH'] = udunits2_xml_path
 
 
-def create_output_summary(opath):
-    """main function to create summary output"""
-
-    json_summary_out, cf_errors_out = {}, {}
-    files = output_directory.return_files_in_directory_tree(opath)
-    for file in files:
-        if file.endswith("_result.json"):
-            json_summary_out[file] = pd.DataFrame()
-            json_summary_out[file] = json_summary_out[file].append(extract_overview_output_json(file), ignore_index=True)
-        elif file.endswith("_CF_result.txt"):
-            cf_errors_out[file] = 0
-            cf_errors_out[file], std_name_table = extracts_error_summary_cf_check(file, cf_errors_out[file])
-    return json_summary_out, cf_errors_out
-
-
-def run_checks_on_files(ifiles):
-    for check in ['atmodat', 'CF']:
-        for f in os.listdir('tmp/' + check):
-            os.remove('tmp/' + check+'/' + f)
-    run_checks(ifiles, False, ['atmodat', 'CF'], 'auto', 'tmp/',
-               '/home/jkretzs/work/scripts/python/atmodat/atmodat_standard_checker/')
-    json_summary, cf_errors = create_output_summary('tmp/')
+def run_checks_on_files(tmpdir, ifiles):
+    atmodat_base_path = os.path.join(str(Path(__file__).resolve().parents[2]), '')
+    run_checks(ifiles, False, ['atmodat', 'CF'], 'auto', tmpdir, atmodat_base_path)
+    json_summary, cf_errors = create_output_summary(tmpdir)
 
     passed_checks = {}
     for file_json in json_summary.keys():
@@ -48,9 +35,33 @@ def run_checks_on_files(ifiles):
     return passed_checks
 
 
-def test_expected_attributes_present():
-    file_list = glob.glob('demo_data/*.nc')
-    passed_json_checks = run_checks_on_files(file_list)
+def create_output_summary(opath):
+    """main function to create summary output"""
+
+    json_summary_out, cf_errors_out, incorrect_formula_term_error = {}, {}, None
+    files = output_directory.return_files_in_directory_tree(opath)
+    for file in files:
+        if file.endswith("_result.json"):
+            json_summary_out[file] = pd.DataFrame()
+            json_summary_out[file] = json_summary_out[file].append(extract_overview_output_json(file),
+                                                                   ignore_index=True)
+        elif file.endswith("_CF_result.txt"):
+            cf_errors_out[file] = 0
+            cf_errors_out[file], std_name_table, incorrect_formula_term_error = \
+                extracts_error_summary_cf_check(file, cf_errors_out[file], incorrect_formula_term_error)
+    return json_summary_out, cf_errors_out
+
+
+def test_expected_attributes_present(tmpdir):
+    for check in ['atmodat', 'CF']:
+        tmpdir.mkdir(check)
+    tmp_dir_test = os.path.join(str(tmpdir), '')
+
+    tmpdir_demo = tmpdir.mkdir('demo_data')
+    git_url = 'https://github.com/AtMoDat/demo_data.git'
+    Repo.clone_from(git_url, tmpdir_demo)
+    file_list = [os.path.join(str(tmpdir_demo), f) for f in os.listdir(tmpdir_demo) if f.endswith('.nc')]
+    passed_json_checks = run_checks_on_files(tmp_dir_test, file_list)
 
     file_check = {}
     file_check['ALL_ATMODAT_ATTRIBUTES.nc'] = {}
