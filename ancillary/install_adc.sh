5ebf9b74ad4bb6cf6223d71386273e9a3250c476
atmodatcode <79904507+atmodatcode@users.noreply.github.com>
2022-02-14 13:36:07 +0100

    Update install_adc.sh
    adjusted installation script to new atmodat checker installation readme.

--- ancillary/install_adc.sh
+++ ancillary/install_adc.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/bash -i
+
+
+## usage: ./install_adc.sh mycustomenvname 
+## mycustomenvname: specify a custom environment name. If not specified, the name will be atmodat.
+
+if [ $# -gt 0 ] ; then
+   new_env=$1
+   mkdir -p $new_env;cd $new_env
+fi
+
 
 git clone https://github.com/AtMoDat/atmodat_data_checker.git
 cd atmodat_data_checker || exit
@@ -14,18 +24,25 @@ echo "Installing atmodat checker in environment" "{$env_name}"
 
 conda env create         # environment name is retrieved from environment.yml
 
-# shellcheck disable=SC1090
-source ~/anaconda3/bin/activate "$env_name"      #-- equivalent to conda activate ${env_name} in terminal
-#source ~/anaconda3/etc/profile.d/conda.sh
 
+conda activate $env_name
+# shellcheck disable=SC1090
+#source ~/anaconda3/bin/activate "$env_name"      #-- equivalent to conda activate ${env_name} in terminal
+#source ~/anaconda3/etc/profile.d/conda.sh
+conda env list
+
+conda config --add channels conda-forge
+
+mamba install --yes --file mamba_requirements.txt
 pip install -U -e .
+
 git submodule init
-git submodule update
+git submodule update  --remote --recursive
 
 
 #=== install jupyterlab and create ipykernel with name of the environment
 
 mamba install --yes -c conda-forge jupyterlab
-python -m ipykernel install --user --name "{$env_name}"kernel
+mamba install --yes -c conda-forge cdo nco
 
-
+python -m ipykernel install --user --name "${$env_name}"kernel
