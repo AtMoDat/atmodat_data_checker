c8421644ce674f32aaf7242ea76ca9f8bc9306cf
Jan Kretzschmar <jan.kretzschmar@uni-leipzig.de>
2022-04-07 17:18:07 +0200

    Some small fixes to previous commit

--- atmodat_checklib/utils/env_util.py
+++ atmodat_checklib/utils/env_util.py
@@ -2,28 +2,33 @@ import os
 from pathlib import Path
 import platform
 
-UDUNITS_PATH_WINDOWS=['share', 'udunits']
-UDUNITS_PATH_LINUX=['Library', 'share', 'udunits']
+UDUNITS_PATH_LINUX = ['share', 'udunits']
+UDUNITS_PATH_WINDOWS = ['Library', 'share', 'udunits']
+
 
 def set_env_variables():
 
-    # Verify we are in atmodat conda environment
-    if not os.environ["UDUNITS2_XML_PATH"] :
-        
-        if platform.system() == 'Windows' :
-            udunits_local_path=UDUNITS_PATH_WINDOWS
-        else :
-            udunits_local_path=UDUNITS_PATH_LINUX
-            
-        for binpath in os.environ["PATH"].split(':') :
-            searchpath=binpath.split(os.path.sep)[:-1]
-            if os.path.isfile(os.path.join([searchpath]+ udunits_local_path)):
-                udunits2_xml_path_out=os.path.join([binpath]+ udunits_local_path)
+    try:
+        os.environ["UDUNITS2_XML_PATH"]
+    except KeyError:
+        if platform.system() == 'Windows':
+            udunits_local_path = UDUNITS_PATH_WINDOWS
+        else:
+            udunits_local_path = UDUNITS_PATH_LINUX
+
+        udunits2_xml_path_out = None
+        for binpath in os.environ["PATH"].split(':'):
+            searchpath = Path(binpath).parents[0]
+            udunits_search_path = os.path.join(searchpath, *udunits_local_path)
+            if os.path.isdir(udunits_search_path):
+                udunits2_xml_path_out = os.path.join(udunits_search_path, 'udunits2.xml')
                 break
+
         if not udunits2_xml_path_out:
             raise RuntimeError("Could not find udunits xml path")
-    else :
-        udunits2_xml_path_out=os.environ["UDUNITS2_XML_PATH"]
+
+    else:
+        udunits2_xml_path_out = os.environ["UDUNITS2_XML_PATH"]
 
     # PYESSV_ARCHIVE_PATH
     atmodat_cv_base_path = str(Path(__file__).resolve().parents[1])
