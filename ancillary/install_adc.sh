720116f06f5c28ad2a0464add385073f309bd9bf
atmodatcode <79904507+atmodatcode@users.noreply.github.com>
2022-05-19 13:05:13 +0200

    Update install_adc.sh

--- ancillary/install_adc.sh
+++ ancillary/install_adc.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-git clone https://github.com/AtMoDat/atmodat_data_checker.git
-cd atmodat_data_checker || exit
-
-
 if [ $# -gt 0 ] ; then
    new_env=$1
+   git clone https://github.com/AtMoDat/atmodat_data_checker.git $new_env
+   cd $new_env || exit
    sed -i.bak "s/atmodat/${new_env}/" environment.yml
+else
+   git clone https://github.com/AtMoDat/atmodat_data_checker.git
+   cd atmodat_data_checker || exit
 fi
 
 env_name=$(sed -n 2p environment.yml |cut -d: -f2|tr -d ' ')
-echo "Installing atmodat checker in environment" "{$env_name}"
+echo "Installing atmodat checker in environment" "$env_name"
 
 conda env create         # environment name is retrieved from environment.yml
 
-# shellcheck disable=SC1090
 source ~/anaconda3/bin/activate "$env_name"      #-- equivalent to conda activate ${env_name} in terminal
-#source ~/anaconda3/etc/profile.d/conda.sh
-
+mamba install --yes --file mamba_requirements.txt
 pip install -U -e .
 git submodule init
-git submodule update
+git submodule update --remote --recursive
 
 
 #=== install jupyterlab and create ipykernel with name of the environment
-
+#=== also install cdo and nco
 mamba install --yes -c conda-forge jupyterlab
-python -m ipykernel install --user --name "{$env_name}"kernel
+mamba install --yes -c conda-forge cdo nco
 
+python -m ipykernel install --user --name "$env_name"kernel
 
+echo "finished installing $env_name
+      you can now activate the atmodat checker environment with
+      conda activate $env_name"
