c2705dd10e981463b9179287b943e5b10900e64f
atmodatcode <79904507+atmodatcode@users.noreply.github.com>
2024-09-16 19:57:19 +0200

    Suppress underscore error..minor edit (#133)
    * suppress inclusion of CF checker errors in short_summary.txt related to leading underscores in attribute name
    
    * Update Python version from 3.6 to 3.8 in GitHub Actions
    
    * Correct flake8 linter errors
    
    * changed pytest.yml to be compatible with the Node.js 20 environment
    
    * removed another linter error
    
    * removed another linter error
    
    * updated valid CF versions in run_checks.py
    
    * minor edit in short summary

--- atmodat_checklib/utils/summary_creation_util.py
+++ atmodat_checklib/utils/summary_creation_util.py
@@ -144,8 +144,8 @@ def write_short_summary(json_summary, cf_version, cf_errors, cf_warns, cf_to_be_
                         f"https://github.com/AtMoDat/atmodat_data_checker#known-issues )\n")
             if cf_to_be_ignored_errors_in.get('invalid_attribute_name', False):
                 f.write(f"CF checker errors: {str(cf_errors)} (Ignoring errors related to the leading underscore "
-                        f"in the attribute _CoordinateAxisType, which, according to CF convention 2.3 "
-                        f"(Naming Conventions), is recommended but not prohibited.)\n")
+                        f"in the attribute _CoordinateAxisType, which, according to CF convention section "
+                        f"Naming Conventions, is recommended but not prohibited.)\n")
             if (not cf_to_be_ignored_errors_in.get('formula_terms', False)
                     and not cf_to_be_ignored_errors_in.get('invalid_attribute_name', False)):
                 f.write(f"CF checker errors: {str(cf_errors)}\n")
